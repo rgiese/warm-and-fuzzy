f907cf2aa3e80085aeaa3104e1c5d06dfc67f53e
Robin Giese <robin@sarahandrobin.com>
2019-04-06 23:22:41 -0700

    Return JSON from Function

--- cloud/functions/ParticleWebhook_Status/index.ts
+++ cloud/functions/ParticleWebhook_Status/index.ts
@@ -10,12 +10,14 @@ const httpTrigger: AzureFunction = async function(
   if (name) {
     context.res = {
       // status: 200, /* Defaults to 200 */
-      body: "Hello " + (req.query.name || req.body.name),
+      body: { name }
     };
   } else {
     context.res = {
       status: 400,
-      body: "Please pass a name on the query string or in the request body",
+      body: {
+        error: "Please pass a name on the query string or in the request body",
+      }
     };
   }
 };
