ec7e7ed7e921b8f33f9fef9b37a849e238a42974
Robin Giese <robin@sarahandrobin.com>
2019-06-21 11:21:37 -0700

    Trivial GET API

---
+++ cloud/functions/GetConfiguration/index.ts
@@ -0,0 +1,56 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+
+import { AzureTableStorage } from "../common/azureTableStorage";
+
+import { DeviceConfiguration } from "../schema/deviceConfiguration";
+
+const tableService = new AzureTableStorage();
+
+const httpTrigger: AzureFunction = async function(
+  context: Context,
+  req: HttpRequest
+): Promise<any> {
+  try {
+    // Retrieve device configuration data
+    const deviceConfigurationJson = await tableService.TryRetrieveEntity(
+      "deviceConfig",
+      "default",
+      "17002c001247363333343437"
+    );
+
+    const deviceConfiguration = new DeviceConfiguration(context, deviceConfigurationJson);
+
+    // Return success response
+    return {
+      // Provide a compacted rendering of the JSON in the response (by default it's pretty)
+      body: JSON.stringify(
+        {
+          sh: deviceConfiguration.setPointHeat,
+          sc: deviceConfiguration.setPointCool,
+          th: deviceConfiguration.threshold,
+          ca: deviceConfiguration.cadence,
+          aa: deviceConfiguration.allowedActions,
+        },
+        undefined,
+        0
+      ),
+
+      // Force JSON content type
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+  } catch (error) {
+    context.log.error(error);
+
+    return {
+      status: 500,
+      body: {
+        error: "exception",
+        details: error,
+      },
+    };
+  }
+};
+
+export default httpTrigger;
