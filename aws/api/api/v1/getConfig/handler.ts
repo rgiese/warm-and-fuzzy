1fc5902905e938d0d48e15dce76ab993d3266e16
Robin Giese <robin@sarahandrobin.com>
2019-06-29 16:54:09 -0700

    Add CORS headers

--- aws/api/api/v1/getConfig/handler.ts
+++ aws/api/api/v1/getConfig/handler.ts
@@ -2,8 +2,14 @@ import { APIGatewayProxyHandler } from "aws-lambda";
 import "source-map-support/register";
 
 export const getConfig: APIGatewayProxyHandler = async event => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
+  };
+
   return {
     statusCode: 200,
+    headers: headers,
     body: JSON.stringify(
       {
         message: "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
