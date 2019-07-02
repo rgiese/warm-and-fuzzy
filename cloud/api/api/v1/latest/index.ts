3e21fd6207c91c322be79120889a8f569f07fab4
Robin Giese <robin@grumpycorp.com>
2019-07-02 10:06:30 -0700

    Add latest values API

---
+++ cloud/api/api/v1/latest/index.ts
@@ -0,0 +1,43 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import "source-map-support/register";
+
+import Authorizations from "../../auth/Authorizations";
+
+import Responses from "../../../common/Responses";
+
+import LatestAction from "../../../types/db/LatestAction";
+import LatestValue from "../../../types/db/LatestValue";
+import DbMapper from "../../../types/db/DbMapper";
+
+export const list: APIGatewayProxyHandler = async event => {
+  // Authorize
+  const authorizations = event.requestContext.authorizer as Authorizations;
+
+  if (!authorizations.AuthorizedTenant || !authorizations.AuthorizedPermissions) {
+    return Responses.noTenantOrPermissions();
+  }
+
+  // Retrieve latest actions
+  let latestActions: LatestAction[] = [];
+
+  for await (const latestAction of DbMapper.query(LatestAction, {
+    tenant: authorizations.AuthorizedTenant,
+  })) {
+    latestActions.push(latestAction);
+  }
+
+  // Retrieve latest Actions
+  let latestValues: LatestValue[] = [];
+
+  for await (const latestValue of DbMapper.query(LatestValue, {
+    tenant: authorizations.AuthorizedTenant,
+  })) {
+    latestValues.push(latestValue);
+  }
+
+  // Build response
+  return Responses.success({
+    actions: latestActions,
+    values: latestValues,
+  });
+};
