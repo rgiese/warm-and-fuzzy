96d2268f667815c32cfd9942a2d795aca4eae955
Robin Giese <robin@grumpycorp.com>
2019-07-09 11:17:50 -0700

    Test database connectivity

--- cloud/api/graphql/index.ts
+++ cloud/api/graphql/index.ts
@@ -3,13 +3,26 @@ import { ApolloServer, makeExecutableSchema } from "apollo-server-lambda";
 import { Resolvers } from "../generated/graphqlTypes";
 import typeDefs from "./schema.graphql";
 
-const logger = { log: (e: any) => console.log(e) };
+import ThermostatConfiguration from "../types/db/ThermostatConfiguration";
+import DbMapper from "../types/db/DbMapper";
 
 const resolvers: Resolvers = {
   Query: {
-    hello: () => "Hello world",
+    hello: async () => {
+      let configs: ThermostatConfiguration[] = [];
+
+      for await (const config of DbMapper.query(ThermostatConfiguration, {
+        tenant: "AmazingHouse",
+      })) {
+        configs.push(config);
+      }
+
+      return JSON.stringify(configs);
+    },
   },
 };
+
+const logger = { log: (e: any) => console.log(e) };
 
 const schema = makeExecutableSchema({ typeDefs, resolvers, logger });
 // also consider inheritResolversFromInterfaces = true
