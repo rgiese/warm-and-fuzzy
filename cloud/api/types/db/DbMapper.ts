36a718be851bde4b77d80bd8c218d38ee6b6a35d
Robin Giese <robin@grumpycorp.com>
2019-07-08 17:00:35 -0700

    Prefix table names with {stage}.

--- cloud/api/types/db/DbMapper.ts
+++ cloud/api/types/db/DbMapper.ts
@@ -3,6 +3,7 @@ import { DataMapper } from "@aws/dynamodb-data-mapper";
 
 const dbMapper = new DataMapper({
   client: new DynamoDB({ region: process.env.DYNAMODB_REGION as string }),
+  tableNamePrefix: process.env.DYNAMODB_TABLE_NAME_PREFIX as string,
 });
 
 export default dbMapper;
