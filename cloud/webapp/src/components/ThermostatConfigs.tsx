0e07f170db128bf34832b6b9b6b8b74b5e1c2734
Robin Giese <robin@grumpycorp.com>
2019-07-10 16:57:14 -0700

    GraphQL client

---
+++ cloud/webapp/src/components/ThermostatConfigs.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import gql from "graphql-tag";
+
+import { ThermostatConfigurationsComponent } from "../generated/graphqlClient";
+
+gql`
+  query ThermostatConfigurations {
+    getThermostatConfigurations {
+      deviceId
+      name
+      allowedActions
+      cadence
+      setPointHeat
+      setPointCool
+      threshold
+    }
+  }
+`;
+
+const ThermostatConfigs: React.FunctionComponent<{}> = (): React.ReactElement => {
+  return (
+    <ThermostatConfigurationsComponent>
+      {({ loading, error, data }): React.ReactElement => {
+        if (loading) return <p>Loading...</p>;
+        if (error || !data || !data.getThermostatConfigurations)
+          return <p>Error :( {JSON.stringify(error)}</p>;
+
+        return (
+          <div>
+            {data.getThermostatConfigurations.map(
+              (thermostatConfiguration): React.ReactElement => {
+                return (
+                  <div key={thermostatConfiguration.deviceId}>
+                    {thermostatConfiguration.name}: {thermostatConfiguration.setPointCool} /{" "}
+                    {thermostatConfiguration.setPointHeat}
+                  </div>
+                );
+              }
+            )}
+          </div>
+        );
+      }}
+    </ThermostatConfigurationsComponent>
+  );
+};
+
+export default ThermostatConfigs;
