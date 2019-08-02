be697575bbb4a9794111e4bc345ebad6edc672b0
Robin Giese <robin@grumpycorp.com>
2019-08-01 22:39:15 -0700

    Use enums instead of strings for permissions

--- packages/api/src/graphql/requiresPermissionDirective.ts
+++ packages/api/src/graphql/requiresPermissionDirective.ts
@@ -3,9 +3,29 @@ import { GraphQLField, defaultFieldResolver } from "graphql";
 import { SchemaDirectiveVisitor } from "graphql-tools";
 
 import { Context } from "./context";
+import * as GraphQL from "../../generated/graphqlTypes";
+
+import { Authorization } from "@grumpycorp/warm-and-fuzzy-shared";
+
+//
+// We can't define GraphQL enum values that exactly match the strings we're using for user permissions
+// so we need to remap them.
+//
+
+const mapGraphQLPermissionToUserPermission = new Map<
+  GraphQL.RequiredPermission,
+  Authorization.Permissions
+>([
+  [GraphQL.RequiredPermission.ReadConfig, Authorization.Permissions.ReadConfig],
+  [GraphQL.RequiredPermission.WriteConfig, Authorization.Permissions.WriteConfig],
+]);
+
+const throwUndefinedPermission = (permission: GraphQL.RequiredPermission): string => {
+  throw new Error(`Unrecognized permission '${permission}'`);
+};
 
 interface RequiresPermissionArgs {
-  permission: string;
+  permission: GraphQL.RequiredPermission;
 }
 
 class RequiresPermissionDirective extends SchemaDirectiveVisitor {
@@ -13,10 +33,13 @@ class RequiresPermissionDirective extends SchemaDirectiveVisitor {
     const { permission } = this.args as RequiresPermissionArgs;
     const { resolve = defaultFieldResolver } = field;
 
+    const requiredPermission =
+      mapGraphQLPermissionToUserPermission.get(permission) || throwUndefinedPermission(permission);
+
     field.resolve = async function(...args) {
       const context = args[2];
 
-      if (!context.AuthorizedPermissions.includes(permission)) {
+      if (!context.AuthorizedPermissions.includes(requiredPermission)) {
         throw new AuthenticationError("Not authorized");
       }
 
