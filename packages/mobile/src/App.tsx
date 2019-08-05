4e736cc94aecf79bace9c00cecc4fec913323c9a
Robin Giese <robin@grumpycorp.com>
2019-08-05 16:00:24 -0700

    Relocate App.tsx

---
+++ packages/mobile/src/App.tsx
@@ -0,0 +1,27 @@
+import { registerRootComponent } from "expo";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class App extends React.Component 
+{
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello from WarmAndFuzzy.</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
+
+registerRootComponent(App);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
