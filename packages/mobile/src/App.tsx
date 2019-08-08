9f0ad17d349f8060aba5c6179e226e1ca81459c9
Robin Giese <robin@grumpycorp.com>
2019-08-08 11:54:10 -0700

    Introduce react-native-paper

--- packages/mobile/src/App.tsx
+++ packages/mobile/src/App.tsx
@@ -3,14 +3,12 @@ import {
   ActivityIndicator,
   Button,
   SafeAreaView,
-  StyleSheet,
   ScrollView,
   View,
   Text,
   StatusBar,
 } from "react-native";
-import { Colors } from "react-native/Libraries/NewAppScreen";
-
+import { Provider as PaperProvider ,DarkTheme } from "react-native-paper";
 import { ApolloProvider } from "react-apollo";
 
 import { ConfigStageName } from "./config";
@@ -19,33 +17,6 @@ import { GlobalAuth } from "./services/Auth";
 import ApolloClient from "./services/ApolloClient";
 
 import LatestValues from "./components/LatestValues";
-
-const styles = StyleSheet.create({
-  body: {
-    backgroundColor: Colors.white,
-  },
-  highlight: {
-    fontWeight: "700",
-  },
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionDescription: {
-    color: Colors.dark,
-    fontSize: 18,
-    fontWeight: "400",
-    marginTop: 8,
-  },
-  sectionTitle: {
-    color: Colors.black,
-    fontSize: 24,
-    fontWeight: "600",
-  },
-});
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props {}
@@ -84,42 +55,39 @@ class App extends React.Component<Props, State> {
 
   public render(): React.ReactElement {
     return (
-      <ApolloProvider client={ApolloClient}>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
-            <View style={styles.body}>
-              <ActivityIndicator
-                size="large"
-                color="#05a5d1"
-                animating={!this.state.hasInitialized}
-              />
-              {this.state.hasInitialized && (
-                <View style={styles.sectionContainer}>
-                  <Text>Stage: {ConfigStageName}</Text>
-                  <Text style={styles.sectionTitle}>Step One</Text>
-                  <Text style={styles.sectionDescription}>
-                    Edit <Text style={styles.highlight}>App.tsx</Text> to change this screen and
-                    then come back to see your edits.
-                  </Text>
-                  {this.state.isAuthenticated ? (
-                    <>
-                      <Text>
-                        You are logged in, {GlobalAuth.UserName}. Your permissions: [
-                        {GlobalAuth.Permissions.join(", ")}]
-                        <LatestValues />
-                      </Text>
-                      <Button title="Log out" onPress={this.handleLogout} />
-                    </>
-                  ) : (
-                    <Button title="Log in" onPress={this.handleLogin} />
-                  )}
-                </View>
-              )}
-            </View>
-          </ScrollView>
-        </SafeAreaView>
-      </ApolloProvider>
+      <PaperProvider theme={DarkTheme}>
+        <ApolloProvider client={ApolloClient}>
+          <StatusBar barStyle="dark-content" />
+          <SafeAreaView>
+            <ScrollView contentInsetAdjustmentBehavior="automatic">
+              <View>
+                <ActivityIndicator
+                  size="large"
+                  color="#05a5d1"
+                  animating={!this.state.hasInitialized}
+                />
+                {this.state.hasInitialized && (
+                  <View>
+                    <Text>Stage: {ConfigStageName}</Text>
+                    {this.state.isAuthenticated ? (
+                      <>
+                        <Text>
+                          You are logged in, {GlobalAuth.UserName}. Your permissions: [
+                          {GlobalAuth.Permissions.join(", ")}]
+                          <LatestValues />
+                        </Text>
+                        <Button title="Log out" onPress={this.handleLogout} />
+                      </>
+                    ) : (
+                      <Button title="Log in" onPress={this.handleLogin} />
+                    )}
+                  </View>
+                )}
+              </View>
+            </ScrollView>
+          </SafeAreaView>
+        </ApolloProvider>
+      </PaperProvider>
     );
   }
 }
