00f558f0c29dcb118b1d76d197675c2ef0315657
Robin Giese <robin@grumpycorp.com>
2019-08-07 23:58:28 -0700

    Update graphql-codegen

--- packages/webapp/src/components/LatestActions.tsx
+++ packages/webapp/src/components/LatestActions.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import gql from "graphql-tag";
 
-import { LatestActionsComponent } from "../generated/graphqlClient";
+import { LatestActionsComponent } from "../../generated/graphqlClient";
 
 gql`
   query LatestActions {
