67f4f478844bd4b698ac0c8c6876dca6aafef3ae
Robin Giese <robin@grumpycorp.com>
2019-08-16 10:21:14 -0700

    Align firmware->cloud config report with cloud->firmware

--- firmware/thermostat/publishers/StatusPublisher.h
+++ firmware/thermostat/publishers/StatusPublisher.h
@@ -43,7 +43,7 @@ public:
         sb.Append("\"");
 
         // Configuration
-        sb.AppendFormat(",\"cc\":{\"sh\":%.1f,\"sc\":%.1f,\"t\":%.2f",
+        sb.AppendFormat(",\"cc\":{\"sh\":%.1f,\"sc\":%.1f,\"th\":%.2f",
                         configuration.SetPointHeat(),
                         configuration.SetPointCool(),
                         configuration.Threshold());
@@ -84,10 +84,10 @@ public:
 
 private:
     static size_t constexpr cchEventData =
-        static_strlen("{'ts':‭4294967295‬,'ser':‭4294967295‬")     // Header
-        + static_strlen(",'t':-100.0,'h':100.0,'ca':'HCR'")                // Status
-        + static_strlen(",cc:{'sh':10.0,'sc':10.0,'t':10.00,'aa':'HCR'}")  // Configuration
-        + static_strlen(",'v':[]}")                                        // Measurements
+        static_strlen("{'ts':‭4294967295‬,'ser':‭4294967295‬")      // Header
+        + static_strlen(",'t':-100.0,'h':100.0,'ca':'HCR'")                 // Status
+        + static_strlen(",cc:{'sh':10.0,'sc':10.0,'th':10.00,'aa':'HCR'}")  // Configuration
+        + static_strlen(",'v':[]}")                                         // Measurements
         + c_cOneWireDevices_Max *
               static_strlen("{'id':'001122334455667788','t':-100.0,'h':100.0},")  // Values from external sensors
         + 4;                                                                      // Safety margin
