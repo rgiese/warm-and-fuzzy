051dbdf021007fdd98b40d80ee61072893f41e5f
Robin Giese <robin@sarahandrobin.com>
2019-08-22 12:47:11 -0700

    Auto-lint

--- packages/shared/src/TypeTools.ts
+++ packages/shared/src/TypeTools.ts
@@ -1,6 +1,6 @@
 export namespace TypeTools {
   // Courtesy of https://stackoverflow.com/questions/46376468/how-to-get-type-of-array-type-values
-  export type ArrayElementType<T extends Array<any>> = T extends (infer U)[] ? U : never;
+  export type ArrayElementType<T extends any[]> = T extends (infer U)[] ? U : never;
 
   // Courtesy of https://stackoverflow.com/questions/45894524/getting-type-of-a-property-of-a-typescript-class-using-keyof-operator
   export type PropType<TObj, TProp extends keyof TObj> = TObj[TProp];
