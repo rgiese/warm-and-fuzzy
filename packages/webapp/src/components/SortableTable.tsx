20538e1edbba74bfaa9e508e682856876e1a525e
Robin Giese <robin@sarahandrobin.com>
2019-08-22 12:47:42 -0700

    Finish up SortableTable

--- packages/webapp/src/components/SortableTable.tsx
+++ packages/webapp/src/components/SortableTable.tsx
@@ -1,48 +1,141 @@
 import React from "react";
 import { Table } from "semantic-ui-react";
 
-class TableFieldDefinition<T extends SortableOfT, SortableOfT extends object> {
-    public constructor(
-      sortField: keyof SortableOfT | undefined,
-      label: string,
-      valueField: keyof T | undefined = sortField
-    ) {
-      this.sortField = sortField;
-      this.valueField = valueField;
-      this.label = label;
-    }
-  
-    public sortField: keyof SortableOfT | undefined;
-    public valueField: keyof T | undefined;
-    public label: string;
-  }
-  
-interface Props<T extends SortableOfT, SortableOfT extends object> {
-    fields: TableFieldDefinition<T, SortableOfT>[];
-    defaultSortField: keyof SortableOfT;
+interface TableData {
+  [key: string]: string | number | Array<string> | Array<number>;
 }
 
-class State<T extends SortableOfT, SortableOfT extends object> {
-  public constructor(props: Props<T, SortableOfT>) {
+export interface TableFieldDefinition<T> {
+  field: keyof T;
+  label: string;
+}
+
+interface Props<T> {
+  data: T[];
+  keyField: keyof T;
+
+  fieldDefinitions: TableFieldDefinition<T>[];
+  defaultSortField: keyof T;
+}
+
+class State<T> {
+  public constructor(props: Props<T>) {
     this.sortOrder = props.defaultSortField;
     this.sortAscending = true;
   }
 
-  sortOrder: keyof SortableOfT;
+  sortOrder: keyof T;
   sortAscending: boolean;
 }
 
-class SortableTable<T extends SortableOfT, SortableOfT extends object> extends React.Component<Props<T, SortableOfT>, State<T, SortableOfT>> {
-  public constructor(props: Props<T, SortableOfT>) {
+class SortableTable<T extends TableData> extends React.Component<Props<T>, State<T>> {
+  public constructor(props: Props<T>) {
     super(props);
-    this.state = new State<T, SortableOfT>(props);
+    this.state = new State<T>(props);
   }
 
+  handleSort = (sortOrder: keyof T) => () => {
+    if (sortOrder !== this.state.sortOrder) {
+      this.setState({ sortOrder, sortAscending: true });
+    } else {
+      this.setState({ sortAscending: !this.state.sortAscending });
+    }
+  };
+
+  isSorted = (sortOrder: keyof T): "ascending" | "descending" | undefined => {
+    if (sortOrder !== this.state.sortOrder) {
+      return undefined;
+    }
+
+    return this.state.sortAscending ? "ascending" : "descending";
+  };
+
+  compareAscending = (lhs: T, rhs: T): number => {
+    const lhsKey = lhs[this.state.sortOrder];
+    const rhsKey = rhs[this.state.sortOrder];
+
+    if (typeof lhsKey !== typeof rhsKey) {
+      // Caller-side TypeScript should have caught this
+      throw new Error(
+        `Sort key types don't match for field ${this.state.sortOrder}: ${lhsKey} / ${rhsKey}`
+      );
+    }
+
+    if (typeof lhsKey === "string" && typeof rhsKey === "string") {
+      return lhsKey.localeCompare(rhsKey);
+    }
+
+    if (typeof lhsKey === "number" && typeof rhsKey === "number") {
+      return lhsKey - rhsKey;
+    }
+
+    if (Array.isArray(lhsKey) && Array.isArray(rhsKey)) {
+      return lhsKey.toString().localeCompare(rhsKey.toString());
+    }
+
+    // Caller-side TypeScript should have caught this
+    throw new Error(`Unsupported type for field ${this.state.sortOrder}`);
+  };
+
+  sortData = (): T[] => {
+    return this.props.data.sort((lhs, rhs): number => {
+      const ascendingResult = this.compareAscending(lhs, rhs);
+      return this.state.sortAscending ? ascendingResult : -1 * ascendingResult;
+    });
+  };
+
   public render(): React.ReactElement {
+    const sortedData = this.sortData();
+
     return (
-        <Table>
-            
-        </Table>
+      <Table sortable basic="very" compact size="small">
+        <Table.Header>
+          <Table.Row>
+            {this.props.fieldDefinitions.map(
+              (fieldDefinition): React.ReactElement => (
+                <Table.HeaderCell
+                  onClick={this.handleSort(fieldDefinition.field)}
+                  sorted={this.isSorted(fieldDefinition.field)}
+                  key={fieldDefinition.label}
+                >
+                  {fieldDefinition.label}
+                </Table.HeaderCell>
+              )
+            )}
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {sortedData.map(
+            (value): React.ReactElement => {
+              return (
+                <Table.Row
+                  key={
+                    Array.isArray(value[this.props.keyField])
+                      ? undefined
+                      : (value[this.props.keyField] as string | number)
+                  }
+                >
+                  {this.props.fieldDefinitions.map(
+                    (fieldDefinition): React.ReactElement => {
+                      const valuePresenter = (v: any) => {
+                        return Array.isArray(v) ? v.join(", ") : v;
+                      };
+
+                      return (
+                        <Table.Cell key={fieldDefinition.field as string}>
+                          {valuePresenter(value[fieldDefinition.field])}
+                        </Table.Cell>
+                      );
+                    }
+                  )}
+                </Table.Row>
+              );
+            }
+          )}
+        </Table.Body>
+      </Table>
     );
   }
 }
+
+export default SortableTable;
--- packages/webapp/src/components/ThermostatConfigurations.tsx
+++ packages/webapp/src/components/ThermostatConfigurations.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { Table } from "semantic-ui-react";
 import gql from "graphql-tag";
 
 import { TypeTools } from "@grumpycorp/warm-and-fuzzy-shared";
+
+import SortableTable, { TableFieldDefinition } from "./SortableTable";
 
 import {
   ThermostatConfigurationsComponent,
   ThermostatConfigurationsQuery,
 } from "../generated/graphqlClient";
 
-import ThermostatConfigurationModel from "./ThermostatConfigurationModal";
+//import ThermostatConfigurationModel from "./ThermostatConfigurationModal";
 
 gql`
   fragment ThermostatConfigurationFields on ThermostatConfiguration {
@@ -43,158 +44,45 @@ type ThermostatConfiguration = TypeTools.ArrayElementType<
   TypeTools.PropType<ThermostatConfigurationsQuery, "getThermostatConfigurations">
 >;
 
-type SortableThermostatConfiguration = Pick<
-  ThermostatConfiguration,
-  "id" | "name" | "streamName" | "setPointHeat" | "setPointCool" | "threshold" | "cadence"
->;
+const tableDefinition: TableFieldDefinition<ThermostatConfiguration>[] = [
+  { field: "id", label: "ID" },
+  { field: "name", label: "Name" },
+  { field: "streamName", label: "Stream Name" },
+  { field: "allowedActions", label: "Allowed actions" },
+  { field: "setPointHeat", label: "Heat to" },
+  { field: "setPointCool", label: "Cool to" },
+  { field: "threshold", label: "Threshold" },
+  { field: "cadence", label: "Cadence" },
+  { field: "availableActions", label: "Available actions" },
+];
 
+const ThermostatConfigs: React.FunctionComponent<{}> = (): React.ReactElement => {
+  return (
+    <ThermostatConfigurationsComponent>
+      {({ loading, error, data }): React.ReactElement => {
+        if (loading) {
+          return <p>Loading...</p>;
+        }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface Props {}
-
-class State {
-  public constructor() {
-    this.sortOrder = "id";
-    this.sortAscending = true;
-  }
-
-  sortOrder: keyof SortableThermostatConfiguration;
-  sortAscending: boolean;
-}
-
-class ThermostatConfigs extends React.Component<Props, State> {
-  public constructor(props: Props) {
-    super(props);
-    this.state = new State();
-  }
-
-  handleSort = (sortOrder: keyof SortableThermostatConfiguration) => () => {
-    if (sortOrder !== this.state.sortOrder) {
-      this.setState({ sortOrder, sortAscending: true });
-    } else {
-      this.setState({ sortAscending: !this.state.sortAscending });
-    }
-  };
-
-  isSorted = (
-    sortOrder: keyof SortableThermostatConfiguration
-  ): "ascending" | "descending" | undefined => {
-    if (sortOrder !== this.state.sortOrder) {
-      return undefined;
-    }
-
-    return this.state.sortAscending ? "ascending" : "descending";
-  };
-
-  public render(): React.ReactElement {
-    return (
-      <ThermostatConfigurationsComponent>
-        {({ loading, error, data }): React.ReactElement => {
-          if (loading) {
-            return <p>Loading...</p>;
-          }
-
-          if (error || !data || !data.getThermostatConfigurations) {
-            return (
-              <p>
-                Error: <pre>{JSON.stringify(error)}</pre>
-              </p>
-            );
-          }
-
-          const thermostatConfigurations = data.getThermostatConfigurations.sort(
-            (lhs, rhs): number => {
-              const lhsKey = lhs[this.state.sortOrder];
-              const rhsKey = rhs[this.state.sortOrder];
-
-              const ascendingResult = ["id", "name", "streamName"].includes(this.state.sortOrder)
-                ? (lhsKey as string).localeCompare(rhsKey as string)
-                : (lhsKey as number) - (rhsKey as number);
-
-              return this.state.sortAscending ? ascendingResult : -1 * ascendingResult;
-            }
-          );
-
-          const tableDefinition: TableFieldDefinition<
-            ThermostatConfiguration,
-            SortableThermostatConfiguration
-          >[] = [
-            new TableFieldDefinition("id", "ID"),
-            new TableFieldDefinition("name", "Name"),
-            new TableFieldDefinition("streamName", "Stream Name"),
-            new TableFieldDefinition(undefined, "Allowed actions", "allowedActions"),
-            new TableFieldDefinition("setPointHeat", "Heat to"),
-            new TableFieldDefinition("setPointCool", "Cool to"),
-            new TableFieldDefinition("threshold", "Threshold"),
-            new TableFieldDefinition("cadence", "Cadence"),
-            new TableFieldDefinition(undefined, "Available actions", "availableActions"),
-          ];
-
+        if (error || !data || !data.getThermostatConfigurations) {
           return (
-            <Table sortable basic="very" compact size="small">
-              <Table.Header>
-                <Table.Row>
-                  {tableDefinition.map(
-                    (fieldDefinition): React.ReactElement => {
-                      return fieldDefinition.sortField !== undefined ? (
-                        <Table.HeaderCell
-                          onClick={this.handleSort(fieldDefinition.sortField)}
-                          sorted={this.isSorted(fieldDefinition.sortField)}
-                          key={fieldDefinition.label}
-                        >
-                          {fieldDefinition.label}
-                        </Table.HeaderCell>
-                      ) : (
-                        <Table.HeaderCell key={fieldDefinition.label}>
-                          {fieldDefinition.label}
-                        </Table.HeaderCell>
-                      );
-                    }
-                  )}
-                </Table.Row>
-              </Table.Header>
-              <Table.Body>
-                {thermostatConfigurations.map(
-                  (value): React.ReactElement => {
-                    return (
-                      <Table.Row key={value.id}>
-                        {tableDefinition.map(
-                          (fieldDefinition): React.ReactElement => {
-                            const valuePresenter = (v: any) => {
-                              return Array.isArray(v) ? v.join(", ") : v;
-                            };
-
-                            if (fieldDefinition.sortField) {
-                              return (
-                                <Table.Cell key={fieldDefinition.sortField}>
-                                  {valuePresenter(value[fieldDefinition.sortField])}
-                                </Table.Cell>
-                              );
-                            } else if (fieldDefinition.valueField) {
-                              return (
-                                <Table.Cell key={fieldDefinition.valueField}>
-                                  {valuePresenter(value[fieldDefinition.valueField])}
-                                </Table.Cell>
-                              );
-                            } else {
-                              return <></>;
-                            }
-                          }
-                        )}
-                        <Table.Cell>
-                          <ThermostatConfigurationModel thermostatConfiguration={value} />
-                        </Table.Cell>
-                      </Table.Row>
-                    );
-                  }
-                )}
-              </Table.Body>
-            </Table>
+            <p>
+              Error: <pre>{JSON.stringify(error)}</pre>
+            </p>
           );
-        }}
-      </ThermostatConfigurationsComponent>
-    );
-  }
-}
+        }
+
+        return (
+          <SortableTable
+            data={data.getThermostatConfigurations}
+            fieldDefinitions={tableDefinition}
+            keyField="id"
+            defaultSortField="name"
+          />
+        );
+      }}
+    </ThermostatConfigurationsComponent>
+  );
+};
 
 export default ThermostatConfigs;
