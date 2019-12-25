724ac617732862e1ce3eb94d3b3e0848284347d1
Robin Giese <robin@grumpycorp.com>
2019-12-24 20:29:09 -0800

    Improve time display, button selection

--- packages/webapp/src/components/ThermostatSettingBean.tsx
+++ packages/webapp/src/components/ThermostatSettingBean.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Button, Icon } from "semantic-ui-react";
 import { observer } from "mobx-react";
+import moment from "moment";
 
 import * as GraphQL from "../generated/graphqlClient";
 
 import { ThermostatSetting } from "@grumpycorp/warm-and-fuzzy-shared-client";
 
-const ThermostatSettingBean: React.FunctionComponent<{ thermostatSetting: ThermostatSetting }> = ({
-  thermostatSetting,
-}): React.ReactElement => {
+const ThermostatSettingBean: React.FunctionComponent<{
+  thermostatSetting: ThermostatSetting;
+  availableActions: GraphQL.ThermostatAction[];
+}> = ({ thermostatSetting, availableActions }): React.ReactElement => {
   const interiorPadding = 10;
 
   const isHeatAllowed = thermostatSetting.allowedActions.includes(GraphQL.ThermostatAction.Heat);
@@ -17,31 +19,50 @@ const ThermostatSettingBean: React.FunctionComponent<{ thermostatSetting: Thermo
     GraphQL.ThermostatAction.Circulate
   );
 
+  const isHoldExpired = (holdUntil?: Date | null): boolean =>
+    holdUntil ? holdUntil.valueOf() < Date.now() : true;
+
+  const formatHoldUntil = (holdUntil?: Date | null): React.ReactFragment =>
+    !isHoldExpired(holdUntil) ? (
+      `until ${moment(holdUntil || Date.now()).fromNow(true)} from now`
+    ) : (
+      <span style={{ fontStyle: "italic" }}>(expired)</span>
+    );
+
   const formatTemperature = (temperature: number): React.ReactFragment => <>{temperature} &deg;C</>;
 
   const inactiveIcon = <Icon name="window minimize outline" />;
 
-  // TODO: Gate actions on availability from configuration
   return (
     <Button.Group style={{ padding: 4 }}>
-      <Button style={{ paddingLeft: interiorPadding, paddingRight: interiorPadding / 2 }}>
-        <Icon name="arrow up" color="red" />
-        {isHeatAllowed ? formatTemperature(thermostatSetting.setPointHeat) : inactiveIcon}
+      <Button style={{ paddingLeft: interiorPadding / 2, paddingRight: 0 }}>
+        {/* Just to provide left padding and equalize padding for remaining buttons */}
       </Button>
 
-      <Button style={{ paddingLeft: interiorPadding / 2, paddingRight: interiorPadding / 2 }}>
-        <Icon name="arrow down" color="blue" />
-        {isCoolAllowed ? formatTemperature(thermostatSetting.setPointCool) : inactiveIcon}
-      </Button>
+      {availableActions.includes(GraphQL.ThermostatAction.Heat) && (
+        <Button style={{ paddingLeft: interiorPadding / 2, paddingRight: interiorPadding / 2 }}>
+          <Icon name="arrow up" color="red" />
+          {isHeatAllowed ? formatTemperature(thermostatSetting.setPointHeat) : inactiveIcon}
+        </Button>
+      )}
 
-      <Button style={{ paddingLeft: interiorPadding / 2, paddingRight: interiorPadding / 2 }}>
-        <Icon name="sync alternate" color="purple" />
-        {isCirculateAllowed ? <Icon name="check" /> : inactiveIcon}
-      </Button>
+      {availableActions.includes(GraphQL.ThermostatAction.Cool) && (
+        <Button style={{ paddingLeft: interiorPadding / 2, paddingRight: interiorPadding / 2 }}>
+          <Icon name="arrow down" color="blue" />
+          {isCoolAllowed ? formatTemperature(thermostatSetting.setPointCool) : inactiveIcon}
+        </Button>
+      )}
+
+      {availableActions.includes(GraphQL.ThermostatAction.Circulate) && (
+        <Button style={{ paddingLeft: interiorPadding / 2, paddingRight: interiorPadding / 2 }}>
+          <Icon name="sync alternate" color="purple" />
+          {isCirculateAllowed ? <Icon name="check" /> : inactiveIcon}
+        </Button>
+      )}
 
       {thermostatSetting.type === GraphQL.ThermostatSettingType.Hold && (
         <Button
-          content={`until ${thermostatSetting.holdUntil?.toISOString()}`}
+          content={formatHoldUntil(thermostatSetting.holdUntil)}
           style={{ paddingLeft: interiorPadding / 2, paddingRight: interiorPadding / 2 }}
         />
       )}
