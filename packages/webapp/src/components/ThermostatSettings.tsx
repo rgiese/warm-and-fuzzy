c143716860ecc5548c5b8c69c70421ec976f7142
Robin Giese <robin@grumpycorp.com>
2019-12-26 10:47:49 -0800

    Web UX for saving setpoints

--- packages/webapp/src/components/ThermostatSettings.tsx
+++ packages/webapp/src/components/ThermostatSettings.tsx
@@ -1,96 +1,120 @@
-import React, { useContext } from "react";
-import { Table } from "semantic-ui-react";
+import React, { useContext, useState } from "react";
+import { Header, Segment } from "semantic-ui-react";
 import { observer } from "mobx-react";
 
-import { ThermostatSettingSchema } from "@grumpycorp/warm-and-fuzzy-shared";
-import { RootStoreContext } from "@grumpycorp/warm-and-fuzzy-shared-client";
+import cloneDeep from "clone-deep";
+
+import { RootStoreContext, ThermostatSettings } from "@grumpycorp/warm-and-fuzzy-shared-client";
 
 import * as GraphQL from "../generated/graphqlClient";
 
 import StoreChecks from "./StoreChecks";
-import ThermostatSettingBean from "./ThermostatSettingBean";
+import ThermostatSettingBean, { IndexedThermostatSetting } from "./ThermostatSettingBean";
 
 const ThermostatSettingsComponent: React.FunctionComponent<{}> = (): React.ReactElement => {
   const rootStore = useContext(RootStoreContext).rootStore;
+  const [isSaving, setIsSaving] = useState(false);
+
+  const compareMaybeDate = (lhs: Date | undefined | null, rhs: Date | undefined | null): number => {
+    if ((lhs?.valueOf() || 0) < (rhs?.valueOf() || 0)) {
+      return -1;
+    }
+    if ((lhs?.valueOf() || 0) > (rhs?.valueOf() || 0)) {
+      return 1;
+    }
+    return 0;
+  };
+
+  const compareMaybeNumber = (
+    lhs: number | null | undefined,
+    rhs: number | null | undefined
+  ): number => {
+    if ((lhs || 0) < (rhs || 0)) {
+      return -1;
+    }
+    if ((lhs || 0) > (rhs || 0)) {
+      return 1;
+    }
+    return 0;
+  };
 
   return (
     <StoreChecks
       requiredStores={[rootStore.thermostatConfigurationStore, rootStore.thermostatSettingsStore]}
     >
-      <Table celled striped structured>
-        {/* Consider making entire table collapsing */}
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Thermostat</Table.HeaderCell>
-            <Table.HeaderCell>Hold</Table.HeaderCell>
-            <Table.HeaderCell colSpan={2}>Schedule</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {rootStore.thermostatSettingsStore.data.map(thermostatSettings => {
-            const thermostatConfiguration = rootStore.thermostatConfigurationStore.findById(
-              thermostatSettings.id
-            );
+      {rootStore.thermostatSettingsStore.data.map(thermostatSettings => {
+        const thermostatConfiguration = rootStore.thermostatConfigurationStore.findById(
+          thermostatSettings.id
+        );
 
-            const holdSetting = thermostatSettings.settings.find(
-              setting =>
-                setting.type === GraphQL.ThermostatSettingType.Hold && setting.allowedActions.length
-            );
+        // Deep-clone settings array so we can sort it here and mutate it in child components;
+        // also inject indexes to map settings back to store definition.
+        // (Deep-cloning may be overkill, but we want to be sure not to mutate the store directly at any point.)
+        const localSettingsArray = cloneDeep(thermostatSettings.settings).map(
+          (thermostatSetting, index): IndexedThermostatSetting => {
+            return { ...thermostatSetting, index };
+          }
+        );
 
-            const scheduledSettings = thermostatSettings.settings.filter(
-              setting => setting.type === GraphQL.ThermostatSettingType.Scheduled
-            );
+        const holdSettings = localSettingsArray
+          .filter(setting => setting.type === GraphQL.ThermostatSettingType.Hold)
+          .sort((lhs, rhs): number => compareMaybeDate(lhs.holdUntil, rhs.holdUntil));
 
-            return (
-              <React.Fragment key={thermostatSettings.id}>
-                <Table.Row>
-                  <Table.Cell collapsing rowSpan={7} verticalAlign="top">
-                    {thermostatConfiguration?.name || thermostatSettings.id}
-                  </Table.Cell>
-                  <Table.Cell collapsing rowSpan={7} verticalAlign="top">
-                    {holdSetting && (
-                      <ThermostatSettingBean
-                        thermostatSetting={holdSetting}
-                        availableActions={thermostatConfiguration?.availableActions || []}
-                      />
-                    )}
-                  </Table.Cell>
-                  <Table.Cell collapsing>{ThermostatSettingSchema.DaysOfWeek[0]}</Table.Cell>
-                  <Table.Cell>
-                    {scheduledSettings
-                      .filter(setting =>
-                        setting.daysOfWeek?.includes(ThermostatSettingSchema.DaysOfWeek[0])
-                      )
-                      .map(setting => (
-                        <ThermostatSettingBean
-                          thermostatSetting={setting}
-                          availableActions={thermostatConfiguration?.availableActions || []}
-                          key={setting.atMinutesSinceMidnight?.toString()}
-                        />
-                      ))}
-                  </Table.Cell>
-                </Table.Row>
-                {ThermostatSettingSchema.DaysOfWeek.slice(1).map(dayOfWeek => (
-                  <Table.Row key={dayOfWeek}>
-                    <Table.Cell collapsing>{dayOfWeek}</Table.Cell>
-                    <Table.Cell>
-                      {scheduledSettings
-                        .filter(setting => setting.daysOfWeek?.includes(dayOfWeek))
-                        .map(setting => (
-                          <ThermostatSettingBean
-                            thermostatSetting={setting}
-                            availableActions={thermostatConfiguration?.availableActions || []}
-                            key={setting.atMinutesSinceMidnight?.toString()}
-                          />
-                        ))}
-                    </Table.Cell>
-                  </Table.Row>
-                ))}
-              </React.Fragment>
-            );
-          })}
-        </Table.Body>
-      </Table>
+        const scheduledSettings = localSettingsArray
+          .filter(setting => setting.type === GraphQL.ThermostatSettingType.Scheduled)
+          .sort((lhs, rhs): number =>
+            compareMaybeNumber(lhs.atMinutesSinceMidnight, rhs.atMinutesSinceMidnight)
+          );
+
+        const orderedSettings = holdSettings.concat(scheduledSettings);
+
+        const onSave = async (
+          updatedThermostatSetting: IndexedThermostatSetting
+        ): Promise<void> => {
+          setIsSaving(true);
+
+          // Create local copy of settings array to update
+          const mutatedSettingsArray = localSettingsArray.slice();
+
+          // Swap in changed element
+          mutatedSettingsArray[updatedThermostatSetting.index] = updatedThermostatSetting;
+
+          // Project array of IndexedThermostatSetting back to ThermostatSetting
+          const mutatedSettings: ThermostatSettings = {
+            ...thermostatSettings,
+            settings: mutatedSettingsArray.map(setting => {
+              const { index, ...rest } = setting;
+              return { ...rest };
+            }),
+          };
+
+          // Update in store
+          await rootStore.thermostatSettingsStore.updateItem(mutatedSettings);
+
+          setIsSaving(false);
+        };
+
+        return (
+          <React.Fragment key={thermostatSettings.id}>
+            <Header as="h3" attached="top">
+              {thermostatConfiguration?.name || thermostatSettings.id}
+            </Header>
+            <Segment attached>
+              {orderedSettings.map((setting, index) => {
+                return (
+                  <ThermostatSettingBean
+                    thermostatSetting={setting}
+                    availableActions={thermostatConfiguration?.availableActions || []}
+                    key={index}
+                    onSave={onSave}
+                    isSaving={isSaving}
+                  />
+                );
+              })}
+            </Segment>
+          </React.Fragment>
+        );
+      })}
     </StoreChecks>
   );
 };
