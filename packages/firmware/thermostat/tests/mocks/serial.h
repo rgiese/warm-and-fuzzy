e8b38a31a87d567fd17746b4600457f00a6cea86
Robin Giese <robin@grumpycorp.com>
2020-01-03 12:47:54 -0800

    Fix up mocks for firmware tests

--- packages/firmware/thermostat/tests/mocks/serial.h
+++ packages/firmware/thermostat/tests/mocks/serial.h
@@ -8,10 +8,25 @@ public:
     }
 
 public:
+    void print(char const* const szData)
+    {
+        puts(szData);
+    }
+
     void println(char const* const szData)
     {
         puts(szData);
         puts("\n");
+    }
+
+    void printf(char const* const szFormat, ...)
+    {
+        va_list args;
+        va_start(args, szFormat);
+
+        vprintf(szFormat, args);
+
+        va_end(args);
     }
 
     void printlnf(char const* const szFormat, ...)
