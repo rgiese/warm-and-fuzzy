553c0a0bf74537b4440675c1df81f546aa0c94ba
Robin Giese <robin@grumpycorp.com>
2020-01-19 19:32:01 -0800

    Thermostat settings list should be ordered by thermostat name

--- packages/webapp/src/containers/ThermostatSettings.tsx
+++ packages/webapp/src/containers/ThermostatSettings.tsx
@@ -9,11 +9,25 @@ import ThermostatSettings from "../components/ThermostatSettings";
 const ThermostatSettingsContainer: React.FunctionComponent<{}> = (): React.ReactElement => {
   const rootStore = useContext(RootStoreContext).rootStore;
 
+  const thermostatConfigurationStore = rootStore.thermostatConfigurationStore;
+  const thermostatSettingsStore = rootStore.thermostatSettingsStore;
+
+  const sortedThermostatSettings = thermostatSettingsStore.data.sort((lhs, rhs): number => {
+    const lhsConfiguration = thermostatConfigurationStore.findById(lhs.id);
+    const rhsConfiguration = thermostatConfigurationStore.findById(rhs.id);
+
+    if (!lhsConfiguration) {
+      return rhsConfiguration ? -1 : 0;
+    }
+
+    return lhsConfiguration.name.localeCompare(rhsConfiguration?.name || "");
+  });
+
   return (
     <StoreChecks
       requiredStores={[rootStore.thermostatConfigurationStore, rootStore.thermostatSettingsStore]}
     >
-      {rootStore.thermostatSettingsStore.data.map(thermostatSettings => (
+      {sortedThermostatSettings.map(thermostatSettings => (
         <ThermostatSettings key={thermostatSettings.id} thermostatSettings={thermostatSettings} />
       ))}
     </StoreChecks>
