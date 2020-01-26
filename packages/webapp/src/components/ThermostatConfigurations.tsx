dd449958517924ba340e5da1065cbaae6a9ae0a9
Robin Giese <robin@grumpycorp.com>
2020-01-26 13:27:30 -0800

    Bug fix: Revert prefs-aware temperature display in configurations table - causes needless GraphQL problems

--- packages/webapp/src/components/ThermostatConfigurations.tsx
+++ packages/webapp/src/components/ThermostatConfigurations.tsx
@@ -1,9 +1,5 @@
-import {
-  RelativeTemperature,
-  ThermostatConfiguration,
-  useRootStore,
-} from "@grumpycorp/warm-and-fuzzy-shared-client";
 import SortableTable, { TableFieldDefinition } from "./SortableTable";
+import { ThermostatConfiguration, useRootStore } from "@grumpycorp/warm-and-fuzzy-shared-client";
 
 import { Authorization } from "@grumpycorp/warm-and-fuzzy-shared";
 import React from "react";
@@ -11,17 +7,19 @@ import StoreChecks from "./StoreChecks";
 import ThermostatConfigurationModal from "./ThermostatConfigurationModal";
 import { observer } from "mobx-react";
 
-type TypedThermostatConfiguration = ThermostatConfiguration & {
-  typedThreshold: RelativeTemperature;
-};
+//
+// Note: We only allow editing the Threshold in Celsius since Celsius is our base unit of measurement.
+//       If we add a display value field here, we'll have to un-cast it when we save an edited value
+//       and that's just more trouble and complexity than it's worth.
+//
 
-const tableDefinition: TableFieldDefinition<TypedThermostatConfiguration>[] = [
+const tableDefinition: TableFieldDefinition<ThermostatConfiguration>[] = [
   { field: "id", label: "ID" },
   { field: "name", label: "Name" },
   { field: "externalSensorId", label: "External sensor" },
   { field: "timezone", label: "Timezone" },
   { field: "streamName", label: "Stream Name" },
-  { field: "typedThreshold", label: "Threshold" },
+  { field: "threshold", label: "Threshold", units: "\u00B0C" },
   { field: "cadence", label: "Cadence", units: "sec" },
   { field: "availableActions", label: "Available actions" },
 ];
@@ -33,16 +31,6 @@ const ThermostatConfigs: React.FunctionComponent = (): React.ReactElement => {
     Authorization.Permissions.WriteConfig
   );
 
-  // Project data
-  const data = rootStore.thermostatConfigurationStore.data.map(
-    (value): TypedThermostatConfiguration => {
-      return {
-        ...value,
-        typedThreshold: new RelativeTemperature(value.threshold),
-      };
-    }
-  );
-
   // eslint-disable-next-line react/no-multi-comp
   const fnBuildEditControl = (value: ThermostatConfiguration): React.ReactElement => (
     <ThermostatConfigurationModal values={value} />
@@ -51,7 +39,7 @@ const ThermostatConfigs: React.FunctionComponent = (): React.ReactElement => {
   return (
     <StoreChecks requiredStores={[rootStore.thermostatConfigurationStore]}>
       <SortableTable
-        data={data}
+        data={rootStore.thermostatConfigurationStore.data}
         defaultSortField="name"
         fieldDefinitions={tableDefinition}
         keyField="id"
