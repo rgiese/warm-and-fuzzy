b3c41dc42171bebebf3b2e401e3ac2b7098223a8
Robin Giese <robin@grumpycorp.com>
2020-01-26 12:50:48 -0800

    Bring web status view up to par with mobile (e.g. show current setpoints) (resolves #98)

--- packages/webapp/src/components/LatestSensorValues.tsx
+++ packages/webapp/src/components/LatestSensorValues.tsx
@@ -3,20 +3,24 @@ import {
   Temperature,
   useRootStore,
 } from "@grumpycorp/warm-and-fuzzy-shared-client";
+import React, { useEffect, useState } from "react";
 import SortableTable, { TableFieldDefinition } from "./SortableTable";
 
-import React from "react";
 import StoreChecks from "./StoreChecks";
+import moment from "moment";
 import { observer } from "mobx-react";
 
 type SensorValue = Omit<LatestSensorValue, "temperature"> & {
+  // Injected fields
   name: string;
+  lastUpdated: string;
+  // Type-converted fields
   temperature: Temperature;
 };
 
 const tableDefinition: TableFieldDefinition<SensorValue>[] = [
   { field: "name", label: "Sensor" },
-  { field: "deviceTime", label: "Time" },
+  { field: "lastUpdated", label: "Last updated" },
   { field: "temperature", label: "Temperature" },
 ];
 
@@ -26,12 +30,37 @@ const LatestSensorValues: React.FunctionComponent = (): React.ReactElement => {
   const latestSensorValuesStore = rootStore.latestSensorValuesStore;
   const sensorConfigurationStore = rootStore.sensorConfigurationStore;
 
+  const [latestRenderTime, setLatestRenderTime] = useState(new Date());
+
+  const refreshStores = (): void => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    rootStore.latestSensorValuesStore.update();
+  };
+
+  useEffect(() => {
+    //
+    // Use latestRenderTime to force a list re-render
+    // every so often in order to update the "Last update a few seconds ago" strings
+    // relative to actual/current time.
+    //
+    const intervalRefreshTimeSince = setInterval(() => setLatestRenderTime(new Date()), 10 * 1000);
+    const intervalRefreshStores = setInterval(() => refreshStores(), 60 * 1000);
+
+    return function cleanup(): void {
+      clearInterval(intervalRefreshTimeSince);
+      clearInterval(intervalRefreshStores);
+    };
+  });
+
   // Project data
   const values = latestSensorValuesStore.data.map(
     (value): SensorValue => {
       return {
         ...value,
+        // Injected fields
         name: sensorConfigurationStore.findById(value.id)?.name ?? value.id,
+        lastUpdated: moment(value.deviceTime).from(latestRenderTime),
+        // Type-converted fields
         temperature: new Temperature(value.temperature),
       };
     }
--- packages/webapp/src/components/LatestThermostatValues.tsx
+++ packages/webapp/src/components/LatestThermostatValues.tsx
@@ -1,24 +1,37 @@
+import * as GraphQL from "../generated/graphqlClient";
+
 import {
   LatestThermostatValue,
   Temperature,
   useRootStore,
 } from "@grumpycorp/warm-and-fuzzy-shared-client";
+import React, { useEffect, useState } from "react";
 import SortableTable, { TableFieldDefinition } from "./SortableTable";
 
-import React from "react";
 import StoreChecks from "./StoreChecks";
+import moment from "moment";
 import { observer } from "mobx-react";
 
-type ThermostatValue = Omit<LatestThermostatValue, "temperature"> & {
+type ThermostatValue = Omit<
+  LatestThermostatValue,
+  "temperature" | "setPointHeat" | "setPointCool"
+> & {
+  // Injected fields
   name: string;
+  lastUpdated: string;
+  // Type-converted fields
   temperature: Temperature;
+  setPointHeat?: Temperature;
+  setPointCool?: Temperature;
 };
 
 const tableDefinition: TableFieldDefinition<ThermostatValue>[] = [
   { field: "name", label: "Thermostat" },
-  { field: "deviceTime", label: "Time" },
+  { field: "lastUpdated", label: "Last updated" },
   { field: "temperature", label: "Temperature" },
   { field: "humidity", label: "Humidity", units: "%" },
+  { field: "setPointHeat", label: "Heat to" },
+  { field: "setPointCool", label: "Cool to" },
   { field: "currentActions", label: "Actions" },
 ];
 
@@ -28,13 +41,47 @@ const LatestThermostatValues: React.FunctionComponent = (): React.ReactElement =
   const latestThermostatValuesStore = rootStore.latestThermostatValuesStore;
   const thermostatConfigurationStore = rootStore.thermostatConfigurationStore;
 
+  const [latestRenderTime, setLatestRenderTime] = useState(new Date());
+
+  const refreshStores = (): void => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    rootStore.latestThermostatValuesStore.update();
+
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    rootStore.thermostatSettingsStore.update();
+  };
+
+  useEffect(() => {
+    //
+    // Use latestRenderTime to force a list re-render
+    // every so often in order to update the "Last update a few seconds ago" strings
+    // relative to actual/current time.
+    //
+    const intervalRefreshTimeSince = setInterval(() => setLatestRenderTime(new Date()), 10 * 1000);
+    const intervalRefreshStores = setInterval(() => refreshStores(), 60 * 1000);
+
+    return function cleanup(): void {
+      clearInterval(intervalRefreshTimeSince);
+      clearInterval(intervalRefreshStores);
+    };
+  });
+
   // Project data
   const values = latestThermostatValuesStore.data.map(
     (value): ThermostatValue => {
       return {
         ...value,
+        // Injected fields
         name: thermostatConfigurationStore.findById(value.id)?.name ?? value.id,
+        lastUpdated: moment(value.deviceTime).from(latestRenderTime),
+        // Type-converted fields
         temperature: new Temperature(value.temperature),
+        setPointHeat: value.allowedActions.includes(GraphQL.ThermostatAction.Heat)
+          ? new Temperature(value.setPointHeat)
+          : undefined,
+        setPointCool: value.allowedActions.includes(GraphQL.ThermostatAction.Cool)
+          ? new Temperature(value.setPointCool)
+          : undefined,
       };
     }
   );
