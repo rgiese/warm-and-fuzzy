4df1460a2ebe08c42fad73cf08885c82cf9488fc
Robin Giese <robin@grumpycorp.com>
2020-05-31 16:02:50 -0700

    Back out "Dial down packaged externals..." since we're now missing all dependencies (duh)

--- packages/api/webpack.config.js
+++ packages/api/webpack.config.js
@@ -1,11 +1,9 @@
 const path = require("path");
 const slsw = require("serverless-webpack");
-const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   mode: slsw.lib.webpack.isLocal ? "development" : "production",
   entry: slsw.lib.entries,
-  externals: [nodeExternals()],
   devtool: "source-map",
   resolve: {
     // c.f. https://github.com/graphql/graphql-js/issues/1272, need to list .mjs prior to .js
