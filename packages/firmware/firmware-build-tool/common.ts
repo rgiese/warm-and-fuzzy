4f9a47074551b4e3e71d97c49f0d42286eb114d2
Robin Giese <robin@grumpycorp.com>
2023-10-28 17:50:37 -0700

    Fix type annotation

--- packages/firmware/firmware-build-tool/common.ts
+++ packages/firmware/firmware-build-tool/common.ts
@@ -1,5 +1,5 @@
 import { join } from "path";
 
-export const getBuiltImagePath = (projectRoot, projectName) => {
+export const getBuiltImagePath = (projectRoot: string, projectName: string) => {
   return join(projectRoot, "build", projectName + ".bin");
 };
