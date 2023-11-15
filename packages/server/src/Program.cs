4a850d2a99501266b0f7aecac8ada6034f27acd4
Robin Giese <robin@grumpycorp.com>
2023-11-14 19:58:57 -0800

    Add skeleton support for settings

--- packages/server/src/Program.cs
+++ packages/server/src/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Parse command line
+if (args.Length < 1)
+{
+    Console.Error.WriteLine("server.exe pathToSettings.json");
+    return -1;
+}
+
+string settingsFileName = args[0];
+
+// Load settings
+WarmAndFuzzyServerSettings serverSettings = WarmAndFuzzyServerSettings.ReadFromFile(settingsFileName);
+
+WarmAndFuzzyDeviceSettings deviceSettings = WarmAndFuzzyDeviceSettings.ReadFromFile(Path.Combine(new string[] { Path.GetDirectoryName(settingsFileName) ?? ".", serverSettings.DeviceSettingsFile }));
+
+foreach (var deviceSetting in deviceSettings.Devices)
+{
+    Console.WriteLine($"  Device {deviceSetting.Key}: {deviceSetting.Value.Name}");
+}
+
+// Run
+Console.WriteLine($"Opening web server at {serverSettings.WebServerPort}...");
+
+Console.WriteLine($"Opening device API server at {serverSettings.DeviceApiServerPort}...");
+
+return 0;
---
+++ packages/server/src/settings/WarmAndFuzzyDeviceSettings.cs
@@ -0,0 +1,45 @@
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+public class WarmAndFuzzyDeviceSetting
+{
+    public required string Name { get; set; }
+
+    public void Validate() { }
+}
+
+public class WarmAndFuzzyDeviceSettings
+{
+    public required Dictionary<string, WarmAndFuzzyDeviceSetting> Devices { get; set; }
+
+    public void Validate()
+    {
+        foreach (var (deviceKey, deviceSetting) in Devices)
+        {
+            // Validate key
+            if (!Regex.IsMatch(deviceKey, @"[0-9A-Fa-f]{12}"))
+            {
+                throw new Exception($"Device key {deviceKey} should be twelve hex digits.");
+            }
+
+            // Validate device setting
+            deviceSetting.Validate();
+        }
+    }
+
+    public static WarmAndFuzzyDeviceSettings ReadFromFile(string settingsFileName)
+    {
+        // Read device settings
+        Console.WriteLine($"Loading device settings from {settingsFileName}...");
+
+        string deviceSettingsJson = File.ReadAllText(settingsFileName);
+
+        WarmAndFuzzyDeviceSettings deviceSettings = JsonSerializer.Deserialize<WarmAndFuzzyDeviceSettings>(deviceSettingsJson) ?? throw new Exception("Could not read device settings");
+
+        deviceSettings.Validate();
+
+        // Commit
+        return deviceSettings;
+    }
+
+}
