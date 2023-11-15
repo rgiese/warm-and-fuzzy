de29e69adc88bf22ec24f6ae3f3513b9ae9a4e45
Robin Giese <robin@grumpycorp.com>
2023-11-15 08:19:40 -0800

    Move to class-scoped program

--- packages/server/src/Program.cs
+++ packages/server/src/Program.cs
@@ -1,64 +1,70 @@
 ﻿using WarmAndFuzzy;
 
-// Parse command line
-if (args.Length < 1)
+internal class Program
 {
-    Console.Error.WriteLine("server.exe pathToServerConfiguration.json");
-    return -1;
-}
-
-string settingsFileName = args[0];
-
-// Load settings
-Store.Initialize(settingsFileName);
-
-Store store = Store.Instance; // grab shortcut
-
-// Run
-Console.WriteLine($"Opening web server at {store.ServerConfiguration.WebServerPort}...");
-
-WebApplication webApplication;
-{
-    // Create builder
-    var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+    private static async Task<int> Main(string[] args)
     {
-        WebRootPath = "webroot"
-    });
-    {
-        builder.Services.AddRazorPages();
+        // Parse command line
+        if (args.Length < 1)
+        {
+            Console.Error.WriteLine("server.exe pathToServerConfiguration.json");
+            return -1;
+        }
+
+        string settingsFileName = args[0];
+
+        // Load settings
+        Store.Initialize(settingsFileName);
+
+        Store store = Store.Instance; // grab shortcut
+
+        // Run
+        Console.WriteLine($"Opening web server at {store.ServerConfiguration.WebServerPort}...");
+
+        WebApplication webApplication;
+        {
+            // Create builder
+            var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+            {
+                WebRootPath = "webroot"
+            });
+            {
+                builder.Services.AddRazorPages();
+            }
+
+            // Build app
+            webApplication = builder.Build();
+
+            // Configure app
+            webApplication.UseStaticFiles();
+
+            webApplication.UseRouting();
+
+            webApplication.MapRazorPages();
+        }
+
+        Console.WriteLine($"Opening device API server at {store.ServerConfiguration.DeviceApiServerPort}...");
+
+        WebApplication deviceApiApplication;
+        {
+            // Create builder
+            var builder = WebApplication.CreateBuilder();
+
+            // Build app
+            deviceApiApplication = builder.Build();
+
+            // Configure API
+            deviceApiApplication.MapGet("/devices", () =>
+            {
+                return store.DeviceConfigurations.Devices;
+            });
+        }
+
+        await Task.WhenAny(
+            webApplication.RunAsync($"http://localhost:{store.ServerConfiguration.WebServerPort}"),
+            deviceApiApplication.RunAsync($"http://localhost:{store.ServerConfiguration.DeviceApiServerPort}")
+        );
+
+        return 0;
     }
-
-    // Build app
-    webApplication = builder.Build();
-
-    // Configure app
-    webApplication.UseStaticFiles();
-
-    webApplication.UseRouting();
-
-    webApplication.MapRazorPages();
 }
-
-Console.WriteLine($"Opening device API server at {store.ServerConfiguration.DeviceApiServerPort}...");
-
-WebApplication deviceApiApplication;
-{
-    // Create builder
-    var builder = WebApplication.CreateBuilder();
-
-    // Build app
-    deviceApiApplication = builder.Build();
-
-    // Configure API
-    deviceApiApplication.MapGet("/devices", () =>
-    {
-        return store.DeviceConfigurations.Devices;
-    });
-}
-
-await Task.WhenAny(
-webApplication.RunAsync($"http://localhost:{store.ServerConfiguration.WebServerPort}"),
-deviceApiApplication.RunAsync($"http://localhost:{store.ServerConfiguration.DeviceApiServerPort}"));
-
-
-return 0;
